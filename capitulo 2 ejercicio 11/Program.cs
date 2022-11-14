401cf7cef25b0c6759f78ee4bfce4b9343c801a8
Hp <Hp@DESKTOP-NVV8KB0>
2022-11-13 23:05:14 -0500

    Agregar archivos de proyecto.

---
+++ capitulo 2 ejercicio 11/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ejercicio11
+{
+    public class program
+    {
+        public void Main()
+        {
+            double pago, metrosp, la, lb, lc, precioc, totprec;
+            Console.WriteLine("DIGITE EL LADO ANCHO DE LA ALBERCA");
+            la = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite el lado largo de la alberca");
+            lb = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite el lado de la alberca");
+            lc = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite el precio del agua para llenar la alberca por metro cubico");
+            precioc = double.Parse(Console.ReadLine());
+
+            metrosp = la *
+                lb * lc;
+            totprec = metrosp * precioc;
+
+            Console.WriteLine($"la cantidad de metros cubicos sera {metrosp} cm3");
+            Console.WriteLine($"la cantidad de metros cubicos sera {totprec} cm3");
+        }
+    }
+}
